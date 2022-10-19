3f6e9939ae6427963a3e6aa9e82708f05429be1c
bianyuanop <chen.me.nan@gmail.com>
2022-10-18 22:30:04 -0400

    update on vampire rate

--- unitlevelup.sdd/luarules/configs/perkfuncs/perkfunclist/godsavesu.lua
+++ unitlevelup.sdd/luarules/configs/perkfuncs/perkfunclist/godsavesu.lua
@@ -2,7 +2,7 @@ return {
     -- this get called 30 times per secs
     onDamage = function (unitID, unitDefID, unitTeam, damage, paralyzer, weaponID, projectileID, attackerID, attackerDefID, attackerTeam)
         local hp = Spring.GetUnitHealth(attackerID)
-        hp = hp + damage
+        hp = hp + math.floor(damage * 0.05)
 
         Spring.SetUnitHealth(attackerID, hp)
     end,
